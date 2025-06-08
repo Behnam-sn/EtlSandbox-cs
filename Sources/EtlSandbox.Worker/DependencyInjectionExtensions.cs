0117f7c4393d6ffd25842f4be2ba2e9cd1a13f3e
Behnam Salehi <behnam.dev@outlook.com>
2025-06-09 00:35:37 +0330

    add `DependencyInjectionExtensions`

---
+++ Sources/EtlSandbox.Worker/DependencyInjectionExtensions.cs
@@ -0,0 +1,60 @@
+using EtlSandbox.Domain.CustomerOrderFlats;
+using EtlSandbox.Domain.Shared;
+using EtlSandbox.Infrastructure;
+using EtlSandbox.Infrastructure.CustomerOrderFlats.Extractors;
+using EtlSandbox.Infrastructure.CustomerOrderFlats.Loaders;
+using EtlSandbox.Infrastructure.CustomerOrderFlats.Repositories;
+using EtlSandbox.Infrastructure.CustomerOrderFlats.Transformers;
+using EtlSandbox.Infrastructure.Shared.ApiClient;
+using EtlSandbox.Persistence;
+using EtlSandbox.Shared.Configurations;
+using EtlSandbox.Worker.CustomerOrderFlats.Workers;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EtlSandbox.Worker;
+
+internal static class DependencyInjectionExtensions
+{
+    internal static void AddConfigureOptions(this IServiceCollection services)
+    {
+        services.ConfigureOptions<ConnectionStringsSetup>();
+    }
+
+    internal static void AddLogs(this IServiceCollection services)
+    {
+        services.AddLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.AddConsole();
+            logging.SetMinimumLevel(LogLevel.Information);
+        });
+    }
+
+    internal static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Entity Framework
+        var connectionString = configuration.GetConnectionString("SqlServer") ??
+                               throw new InvalidOperationException("Connection string 'SqlServer'" + " not found.");
+
+        services.AddDbContext<ApplicationDbContext>(b => b.UseSqlServer(
+            connectionString,
+            bb => { bb.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null); })
+        );
+
+        // Rest Api Client
+        services.AddHttpClient();
+        services.AddScoped<IRestApiClient, FlurlRestApiClient>();
+
+        services.AddScoped<IEtlStateCommandRepository, EtlStateCommandRepository>();
+        services.AddScoped<ICommandRepository<CustomerOrderFlat>, CustomerOrderFlatEfCommandRepository>();
+        services.AddScoped<ITransformer<CustomerOrderFlat>, CustomerOrderFlatTransformer>();
+        services.AddScoped<IExtractor<CustomerOrderFlat>, CustomerOrderFlatRestApiExtractor>();
+        services.AddScoped<ILoader<CustomerOrderFlat>, CustomerOrderFlatSqlBulkCopyLoader>();
+    }
+
+    internal static void AddPresentation(this IServiceCollection services)
+    {
+        services.AddHostedService<InsertCustomerOrderFlatWorker>();
+    }
+}
--- Sources/EtlSandbox.Worker/Program.cs
+++ Sources/EtlSandbox.Worker/Program.cs
@@ -1,51 +1,14 @@
-using EtlSandbox.Domain.CustomerOrderFlats;
-using EtlSandbox.Domain.Shared;
-using EtlSandbox.Infrastructure;
-using EtlSandbox.Infrastructure.CustomerOrderFlats.Extractors;
-using EtlSandbox.Infrastructure.CustomerOrderFlats.Loaders;
-using EtlSandbox.Infrastructure.CustomerOrderFlats.Repositories;
-using EtlSandbox.Infrastructure.CustomerOrderFlats.Transformers;
-using EtlSandbox.Infrastructure.Shared.ApiClient;
-using EtlSandbox.Persistence;
 using EtlSandbox.Shared;
-using EtlSandbox.Shared.Configurations;
-using EtlSandbox.Worker.CustomerOrderFlats.Workers;
-
-using Microsoft.EntityFrameworkCore;
+using EtlSandbox.Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Configuration.AddSharedConfiguration();
 
-builder.Services.AddHttpClient();
-
-builder.Services.ConfigureOptions<ConnectionStringsSetup>();
-
-var connectionString = builder.Configuration.GetConnectionString("SqlServer") ??
-    throw new InvalidOperationException("Connection string 'SqlServer'" + " not found.");
-
-builder.Services.AddDbContext<ApplicationDbContext>(b => b.UseSqlServer(
-    connectionString,
-    bb => { bb.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null); })
-);
-
-builder.Services.AddLogging(logging =>
-{
-    logging.ClearProviders();
-    logging.AddConsole();
-    logging.SetMinimumLevel(LogLevel.Information);
-});
-
-builder.Services.AddScoped<IRestApiClient, FlurlRestApiClient>();
-
-builder.Services.AddScoped<IEtlStateCommandRepository, EtlStateCommandRepository>();
-builder.Services.AddScoped<ICommandRepository<CustomerOrderFlat>, CustomerOrderFlatEfCommandRepository>();
-builder.Services.AddScoped<ITransformer<CustomerOrderFlat>, CustomerOrderFlatTransformer>();
-// builder.Services.AddScoped<IExtractor<CustomerOrderFlat>, CustomerOrderFlatDbExtractor>();
-builder.Services.AddScoped<IExtractor<CustomerOrderFlat>, CustomerOrderFlatRestApiExtractor>();
-builder.Services.AddScoped<ILoader<CustomerOrderFlat>, CustomerOrderFlatSqlBulkCopyLoader>();
-
-builder.Services.AddHostedService<InsertCustomerOrderFlatWorker>();
+builder.Services.AddConfigureOptions();
+builder.Services.AddLogs();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddPresentation();
 
 var host = builder.Build();
 await host.RunAsync();
