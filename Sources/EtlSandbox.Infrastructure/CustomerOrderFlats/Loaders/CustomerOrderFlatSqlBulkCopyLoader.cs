e5fb1b539f1858a7dfe41bc04afb5e7bb6a2aaff
Behnam Salehi <behnam.dev@outlook.com>
2025-06-12 15:02:25 +0330

    Update CustomerOrderFlatSqlBulkCopyLoader.cs

--- Sources/EtlSandbox.Infrastructure/CustomerOrderFlats/Loaders/CustomerOrderFlatSqlBulkCopyLoader.cs
+++ Sources/EtlSandbox.Infrastructure/CustomerOrderFlats/Loaders/CustomerOrderFlatSqlBulkCopyLoader.cs
@@ -13,6 +13,7 @@ namespace EtlSandbox.Infrastructure.CustomerOrderFlats.Loaders;
 public class CustomerOrderFlatSqlBulkCopyLoader : ILoader<CustomerOrderFlat>
 {
     private readonly string _destinationConnectionString;
+
     private readonly ILogger<CustomerOrderFlatSqlBulkCopyLoader> _logger;
 
     public CustomerOrderFlatSqlBulkCopyLoader(IOptions<DatabaseConnections> options, ILogger<CustomerOrderFlatSqlBulkCopyLoader> logger)
@@ -28,28 +29,37 @@ public class CustomerOrderFlatSqlBulkCopyLoader : ILoader<CustomerOrderFlat>
         table.Columns.Add("CustomerName", typeof(string));
         table.Columns.Add("Amount", typeof(decimal));
         table.Columns.Add("RentalDate", typeof(DateTime));
+        table.Columns.Add("Category", typeof(string));
+        table.Columns.Add("UniqId", typeof(int));
+        table.Columns.Add("IsDeleted", typeof(bool));
 
         foreach (var item in data)
         {
-            table.Rows.Add(item.RentalId, item.CustomerName, item.Amount, item.RentalDate);
+            table.Rows.Add(
+                item.RentalId,
+                item.CustomerName,
+                item.Amount,
+                item.RentalDate,
+                item.Category,
+                item.UniqId,
+                item.IsDeleted
+            );
         }
 
         SqlBulkCopy bulkCopy;
         if (transaction?.Connection is SqlConnection sqlConnection)
         {
-            bulkCopy = new SqlBulkCopy(sqlConnection, SqlBulkCopyOptions.Default, (SqlTransaction)transaction);
+            bulkCopy = new(sqlConnection, SqlBulkCopyOptions.Default, (SqlTransaction)transaction);
         }
         else
         {
-            bulkCopy = new SqlBulkCopy(_destinationConnectionString);
+            bulkCopy = new(_destinationConnectionString);
         }
 
         using (bulkCopy)
         {
             bulkCopy.DestinationTableName = "CustomerOrders";
-            _logger.LogInformation("Loading {Count} rows", data.Count);
             await bulkCopy.WriteToServerAsync(table, cancellationToken);
-            _logger.LogInformation("Load completed");
         }
     }
 }
