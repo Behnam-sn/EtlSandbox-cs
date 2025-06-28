547c0ee995b6ba29a46d08bf1314f53166ad3186
Behnam Salehi <behnam.dev@outlook.com>
2025-06-28 10:57:05 +0330

    Rename `EtlSandbox.Worker` to `EtlSandbox.AlphaWorker`

--- Sources/EtlSandbox.AlphaWorker/Scripts/CreateInitialMigration.ps1
+++ Sources/EtlSandbox.AlphaWorker/Scripts/CreateInitialMigration.ps1
@@ -1,11 +1,11 @@
 # Drop Database:
-dotnet ef database drop --project Sources\EtlSandbox.Worker\
+dotnet ef database drop --project Sources\EtlSandbox.AlphaWorker\
 
 # Remove Migrations
-dotnet ef migrations remove --project Sources\EtlSandbox.Worker\
+dotnet ef migrations remove --project Sources\EtlSandbox.AlphaWorker\
 
 # Add Migration
-dotnet ef migrations add Initial --project Sources\EtlSandbox.Worker\
+dotnet ef migrations add Initial --project Sources\EtlSandbox.AlphaWorker\
 
 # Apply Migrations
-dotnet ef database update --project Sources\EtlSandbox.Worker\
+dotnet ef database update --project Sources\EtlSandbox.AlphaWorker\
