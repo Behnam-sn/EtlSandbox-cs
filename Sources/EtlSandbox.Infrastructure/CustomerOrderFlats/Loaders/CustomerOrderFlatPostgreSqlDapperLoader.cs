dd2759313b4f0061e304502eaee4b0503d6635ef
Behnam Salehi <behnam.dev@outlook.com>
2025-06-29 10:52:21 +0330

    Add `BaseDapperLoader`

--- Sources/EtlSandbox.Infrastructure/CustomerOrderFlats/Loaders/CustomerOrderFlatPostgreSqlDapperLoader.cs
+++ Sources/EtlSandbox.Infrastructure/CustomerOrderFlats/Loaders/CustomerOrderFlatPostgreSqlDapperLoader.cs
@@ -1,50 +1,18 @@
-﻿using System.Data;
-
-using Dapper;
-
-using EtlSandbox.Domain.CustomerOrderFlats;
+﻿using EtlSandbox.Domain.CustomerOrderFlats;
 using EtlSandbox.Domain.Shared;
+using EtlSandbox.Infrastructure.Shared.Loaders;
 
 using Microsoft.Extensions.Logging;
 
 namespace EtlSandbox.Infrastructure.CustomerOrderFlats.Loaders;
 
-public sealed class CustomerOrderFlatPostgreSqlDapperLoader : ILoader<CustomerOrderFlat>
+public sealed class CustomerOrderFlatPostgreSqlDapperLoader(ILogger<CustomerOrderFlatPostgreSqlDapperLoader> logger, IUnitOfWork unitOfWork)
+    : BaseDapperLoader<CustomerOrderFlat>(logger, unitOfWork, Sql)
 {
-    private readonly ILogger<CustomerOrderFlatPostgreSqlDapperLoader> _logger;
-
-    private readonly IUnitOfWork _unitOfWork;
-
-    public CustomerOrderFlatPostgreSqlDapperLoader(ILogger<CustomerOrderFlatPostgreSqlDapperLoader> logger, IUnitOfWork unitOfWork)
-    {
-        _logger = logger;
-        _unitOfWork = unitOfWork;
-    }
-
-    public async Task LoadAsync(List<CustomerOrderFlat> data, CancellationToken cancellationToken, IDbTransaction? transaction = null)
-    {
-        if (data.Count == 0)
-        {
-            _logger.LogInformation("No data to load");
-            return;
-        }
-
-        const string sql = """
-                           INSERT INTO "CustomerOrders" 
-                               ("RentalId", "CustomerName", "Amount", "RentalDate", "Category", "UniqId", "IsDeleted") 
-                           VALUES 
-                               (@RentalId, @CustomerName, @Amount, @RentalDate, @Category, @UniqId, @IsDeleted)
-                           """;
-
-        if (transaction is null)
-        {
-            var connection = _unitOfWork.Connection;
-            await connection.ExecuteAsync(sql, data);
-        }
-        else
-        {
-            var connection = transaction.Connection ?? throw new ArgumentNullException(nameof(transaction), "Transaction must have a valid connection.");
-            await connection.ExecuteAsync(sql, data, transaction);
-        }
-    }
+    private const string Sql = """
+                               INSERT INTO "CustomerOrders" 
+                                   ("RentalId", "CustomerName", "Amount", "RentalDate", "Category", "UniqId", "IsDeleted") 
+                               VALUES 
+                                   (@RentalId, @CustomerName, @Amount, @RentalDate, @Category, @UniqId, @IsDeleted)
+                               """;
 }
---
+++ Sources/EtlSandbox.Infrastructure/Shared/Loaders/BaseDapperLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+
+using Dapper;
+
+using EtlSandbox.Domain.Shared;
+
+using Microsoft.Extensions.Logging;
+
+namespace EtlSandbox.Infrastructure.Shared.Loaders;
+
+public abstract class BaseDapperLoader<T> : ILoader<T>
+{
+    private readonly ILogger<BaseDapperLoader<T>> _logger;
+
+    private readonly IUnitOfWork _unitOfWork;
+
+    private readonly string _sql;
+
+    protected BaseDapperLoader(ILogger<BaseDapperLoader<T>> logger, IUnitOfWork unitOfWork, string sql)
+    {
+        _logger = logger;
+        _unitOfWork = unitOfWork;
+        _sql = sql;
+    }
+
+    public async Task LoadAsync(List<T> data, CancellationToken cancellationToken, IDbTransaction? transaction = null)
+    {
+        if (data.Count == 0)
+        {
+            _logger.LogInformation("No data to load");
+            return;
+        }
+
+        if (transaction is null)
+        {
+            var connection = _unitOfWork.Connection;
+            await connection.ExecuteAsync(_sql, data);
+        }
+        else
+        {
+            var connection = transaction.Connection ?? throw new ArgumentNullException(nameof(transaction), "Transaction must have a valid connection.");
+            await connection.ExecuteAsync(_sql, data, transaction);
+        }
+    }
+}
