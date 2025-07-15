a7f8aa2faa5f701375fb077afc54024c4177c653
Behnam Salehi <behnam.dev@outlook.com>
2025-07-15 23:55:04 +0330

    Add `BaseSqlBulkCopyLoader`

--- Sources/EtlSandbox.Infrastructure/CustomerOrderFlats/Loaders/CustomerOrderFlatSqlServerBulkCopyLoader.cs
+++ Sources/EtlSandbox.Infrastructure/CustomerOrderFlats/Loaders/CustomerOrderFlatSqlServerBulkCopyLoader.cs
@@ -1,45 +1,10 @@
-using System.Data;
-
 using EtlSandbox.Domain.CustomerOrderFlats.Entities;
-using EtlSandbox.Domain.Shared;
 using EtlSandbox.Domain.Shared.Options;
-using EtlSandbox.Infrastructure.Shared.Converters;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Logging;
+using EtlSandbox.Infrastructure.Shared.Loaders;
+
 using Microsoft.Extensions.Options;
 
 namespace EtlSandbox.Infrastructure.CustomerOrderFlats.Loaders;
 
-public sealed class CustomerOrderFlatSqlServerBulkCopyLoader : ILoader<CustomerOrderFlat>
-{
-    private readonly string _destinationDatabaseConnectionString;
-
-    private readonly ILogger<CustomerOrderFlatSqlServerBulkCopyLoader> _logger;
-
-    public CustomerOrderFlatSqlServerBulkCopyLoader(IOptions<DatabaseConnections> options, ILogger<CustomerOrderFlatSqlServerBulkCopyLoader> logger)
-    {
-        _destinationDatabaseConnectionString = options.Value.Destination;
-        _logger = logger;
-    }
-
-    public async Task LoadAsync(List<CustomerOrderFlat> data, CancellationToken cancellationToken, IDbTransaction? transaction = null)
-    {
-        var table = DataTableConverter.ToDataTable(data);
-
-        SqlBulkCopy bulkCopy;
-        if (transaction?.Connection is SqlConnection sqlConnection)
-        {
-            bulkCopy = new(sqlConnection, SqlBulkCopyOptions.Default, (SqlTransaction)transaction);
-        }
-        else
-        {
-            bulkCopy = new(_destinationDatabaseConnectionString);
-        }
-
-        using (bulkCopy)
-        {
-            bulkCopy.DestinationTableName = "CustomerOrders";
-            await bulkCopy.WriteToServerAsync(table, cancellationToken);
-        }
-    }
-}
+public sealed class CustomerOrderFlatSqlServerBulkCopyLoader(IOptions<DatabaseConnections> databaseConnectionsOptions)
+    : BaseSqlBulkCopyLoader<CustomerOrderFlat>(databaseConnectionsOptions, tableName: "CustomerOrders");
---
+++ Sources/EtlSandbox.Infrastructure/Shared/Loaders/BaseSqlBulkCopyLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+
+using EtlSandbox.Domain.Shared;
+using EtlSandbox.Domain.Shared.Options;
+using EtlSandbox.Infrastructure.Shared.Converters;
+
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+
+namespace EtlSandbox.Infrastructure.Shared.Loaders;
+
+public abstract class BaseSqlBulkCopyLoader<T> : ILoader<T>
+{
+    private readonly string _connectionString;
+    private readonly string _tableName;
+
+    protected BaseSqlBulkCopyLoader(IOptions<DatabaseConnections> databaseConnectionsOptions, string tableName)
+    {
+        _connectionString = databaseConnectionsOptions.Value.Destination;
+        _tableName = tableName;
+    }
+
+    public async Task LoadAsync(List<T> data, CancellationToken cancellationToken, IDbTransaction? transaction = null)
+    {
+        var table = DataTableConverter.ToDataTable(data);
+
+        SqlBulkCopy bulkCopy;
+        if (transaction?.Connection is SqlConnection sqlConnection)
+        {
+            bulkCopy = new(sqlConnection, SqlBulkCopyOptions.Default, (SqlTransaction)transaction);
+        }
+        else
+        {
+            bulkCopy = new(_connectionString);
+        }
+
+        using (bulkCopy)
+        {
+            bulkCopy.DestinationTableName = _tableName;
+            await bulkCopy.WriteToServerAsync(table, cancellationToken);
+        }
+    }
+}
