af31b96cc8c7e829f9d018a6f3df8c38ef5ee44b
Behnam Salehi <behnam.dev@outlook.com>
2025-08-07 01:44:43 +0330

    Implement `CustomerOrderFlatWebApiSourceRepository `

--- Sources/Infrastructures/EtlSandbox.Infrastructure/CustomerOrderFlats/Repositories/CustomerOrderFlatWebApiSourceRepository.cs
+++ Sources/Infrastructures/EtlSandbox.Infrastructure/CustomerOrderFlats/Repositories/CustomerOrderFlatWebApiSourceRepository.cs
@@ -1,12 +1,28 @@
 ﻿using EtlSandbox.Domain.CustomerOrderFlats.Entities;
+using EtlSandbox.Domain.Shared;
 using EtlSandbox.Domain.Shared.Repositories;
 
 namespace EtlSandbox.Infrastructure.CustomerOrderFlats.Repositories;
 
 public sealed class CustomerOrderFlatWebApiSourceRepository : ISourceRepository<CustomerOrderFlat>
 {
-    public Task<long> GetLastItemIdAsync()
+    private readonly string _baseUrl;
+
+    private readonly IRestApiClient _restApiClient;
+
+    public CustomerOrderFlatWebApiSourceRepository(string baseUrl, IRestApiClient restApiClient)
     {
-        throw new NotImplementedException();
+        _baseUrl = baseUrl;
+        _restApiClient = restApiClient;
+    }
+
+    public async Task<long> GetLastItemIdAsync(CancellationToken cancellationToken = default)
+    {
+        var item = await _restApiClient.GetAsync<long>(
+            baseUrl: _baseUrl,
+            path: "CustomerOrderFlats/GetLastItemId",
+            cancellationToken: cancellationToken
+        );
+        return item;
     }
 }
