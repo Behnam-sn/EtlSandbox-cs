dd7e4a619a4851049a8b175117b7b7547ef3e55b
Behnam Salehi <behnam.dev@outlook.com>
2025-08-10 14:38:48 +0330

    Create `EtlSandbox.Infrastructure.Apollo` project

---
+++ Sources/Infrastructures/EtlSandbox.Infrastructure.Apollo/schema.sql
@@ -0,0 +1,15 @@
+﻿CREATE
+DATABASE SakilaFlat;
+
+CREATE TABLE CustomerOrderFlats
+(
+    Id           Int64,
+    RentalId     Int64,
+    CustomerName Nullable(String),
+    Amount       Decimal(10, 2),
+    RentalDate   String,
+    Category     Nullable(String),
+    IsDeleted    UInt8
+) ENGINE = MergeTree()
+ORDER BY (Id);
+
