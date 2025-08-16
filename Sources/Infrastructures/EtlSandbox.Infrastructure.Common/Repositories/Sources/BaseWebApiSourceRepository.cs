b8597858f6bcfa38554e81634cdaacb03560660d
Behnam Salehi <behnam.dev@outlook.com>
2025-08-16 17:40:38 +0330

    Create `BaseWebApiSourceRepository`

---
+++ Sources/Infrastructures/EtlSandbox.Infrastructure.Common/Repositories/Sources/BaseWebApiSourceRepository.cs
@@ -0,0 +1,30 @@
+﻿using EtlSandbox.Domain.Common;
+using EtlSandbox.Domain.Common.Repositories;
+
+namespace EtlSandbox.Infrastructure.Common.Repositories.Sources;
+
+public abstract class BaseWebApiSourceRepository<T> : ISourceRepository<T>
+    where T : class, IEntity
+{
+    private readonly string _baseUrl;
+
+    private readonly IRestApiClient _restApiClient;
+
+    protected BaseWebApiSourceRepository(string baseUrl, IRestApiClient restApiClient)
+    {
+        _baseUrl = baseUrl;
+        _restApiClient = restApiClient;
+    }
+
+    protected abstract string Path { get; }
+
+    public async Task<long> GetLastItemIdAsync(CancellationToken cancellationToken = default)
+    {
+        var item = await _restApiClient.GetAsync<long>(
+            baseUrl: _baseUrl,
+            path: $"{Path}/GetLastItemId",
+            cancellationToken: cancellationToken
+        );
+        return item;
+    }
+}
--- Sources/Infrastructures/EtlSandbox.Infrastructure/CustomerOrderFlats/Repositories/Sources/CustomerOrderFlatWebApiSourceRepository.cs
+++ Sources/Infrastructures/EtlSandbox.Infrastructure/CustomerOrderFlats/Repositories/Sources/CustomerOrderFlatWebApiSourceRepository.cs
@@ -1,28 +1,12 @@
 ﻿using EtlSandbox.Domain.Common;
 using EtlSandbox.Domain.Common.Repositories;
 using EtlSandbox.Domain.CustomerOrderFlats.Entities;
+using EtlSandbox.Infrastructure.Common.Repositories.Sources;
 
 namespace EtlSandbox.Infrastructure.CustomerOrderFlats.Repositories.Sources;
 
-public sealed class CustomerOrderFlatWebApiSourceRepository : ISourceRepository<CustomerOrderFlat>
+public sealed class CustomerOrderFlatWebApiSourceRepository(string baseUrl, IRestApiClient restApiClient)
+    : BaseWebApiSourceRepository<CustomerOrderFlat>(baseUrl, restApiClient)
 {
-    private readonly string _baseUrl;
-
-    private readonly IRestApiClient _restApiClient;
-
-    public CustomerOrderFlatWebApiSourceRepository(string baseUrl, IRestApiClient restApiClient)
-    {
-        _baseUrl = baseUrl;
-        _restApiClient = restApiClient;
-    }
-
-    public async Task<long> GetLastItemIdAsync(CancellationToken cancellationToken = default)
-    {
-        var item = await _restApiClient.GetAsync<long>(
-            baseUrl: _baseUrl,
-            path: "CustomerOrderFlats/GetLastItemId",
-            cancellationToken: cancellationToken
-        );
-        return item;
-    }
+    protected override string Path => "CustomerOrderFlats";
 }
