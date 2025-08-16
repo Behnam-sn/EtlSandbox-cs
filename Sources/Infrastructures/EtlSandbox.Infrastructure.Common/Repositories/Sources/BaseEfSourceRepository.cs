2b3dbbe70cc7d6f010180ff6788cf132c1870937
Behnam Salehi <behnam.dev@outlook.com>
2025-08-16 17:38:41 +0330

    Create `BaseEfSourceRepository`

---
+++ Sources/Infrastructures/EtlSandbox.Infrastructure.Common/Repositories/Sources/BaseEfSourceRepository.cs
@@ -0,0 +1,28 @@
+﻿using EtlSandbox.Domain.Common;
+using EtlSandbox.Domain.Common.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EtlSandbox.Infrastructure.Common.Repositories.Sources;
+
+public abstract class BaseEfSourceRepository<T> : ISourceRepository<T>
+    where T : class, IEntity
+{
+    private readonly DbSet<T> _dbSet;
+
+    protected BaseEfSourceRepository(DbContext dbContext)
+    {
+        _dbSet = dbContext.Set<T>();
+    }
+
+    public async Task<long> GetLastItemIdAsync(CancellationToken cancellationToken = default)
+    {
+        // Todo: change this into max
+        var lastItemId = await _dbSet
+            .AsNoTracking()
+            .OrderByDescending(item => item.Id)
+            .Select(item => (long?)item.Id)
+            .FirstOrDefaultAsync(cancellationToken);
+        return lastItemId ?? 0;
+    }
+}
--- Sources/Infrastructures/EtlSandbox.Infrastructure/CustomerOrderFlats/Repositories/Sources/CustomerOrderFlatEfSourceRepository.cs
+++ Sources/Infrastructures/EtlSandbox.Infrastructure/CustomerOrderFlats/Repositories/Sources/CustomerOrderFlatEfSourceRepository.cs
@@ -1,27 +1,9 @@
-﻿using EtlSandbox.Domain.Common.Repositories;
-using EtlSandbox.Domain.CustomerOrderFlats.Entities;
+﻿using EtlSandbox.Domain.CustomerOrderFlats.Entities;
+using EtlSandbox.Infrastructure.Common.Repositories;
+using EtlSandbox.Infrastructure.Common.Repositories.Sources;
 
 using Microsoft.EntityFrameworkCore;
 
 namespace EtlSandbox.Infrastructure.CustomerOrderFlats.Repositories.Sources;
 
-public sealed class CustomerOrderFlatEfSourceRepository : ISourceRepository<CustomerOrderFlat>
-{
-    private readonly DbSet<CustomerOrderFlat> _dbSet;
-
-    public CustomerOrderFlatEfSourceRepository(DbContext dbContext)
-    {
-        _dbSet = dbContext.Set<CustomerOrderFlat>();
-    }
-
-    public async Task<long> GetLastItemIdAsync(CancellationToken cancellationToken = default)
-    {
-        // Todo: change this into max
-        var lastItemId = await _dbSet
-            .AsNoTracking()
-            .OrderByDescending(item => item.Id)
-            .Select(item => (long?)item.Id)
-            .FirstOrDefaultAsync(cancellationToken);
-        return lastItemId ?? 0;
-    }
-}
+public sealed class CustomerOrderFlatEfSourceRepository(DbContext dbContext) : BaseEfSourceRepository<CustomerOrderFlat>(dbContext);
