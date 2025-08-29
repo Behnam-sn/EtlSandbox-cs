9f24209f122d17d74b0912252cf0152ae5a11f5a
Behnam Salehi <behnam.dev@outlook.com>
2025-08-29 23:14:35 +0330

    Clean up

--- Sources/Cores/EtlSandbox.Domain/Common/Options/GlobalSettings.cs
+++ Sources/Cores/EtlSandbox.Domain/Common/Options/GlobalSettings.cs
@@ -2,13 +2,9 @@
 
 public sealed class GlobalSettings
 {
-    public int BatchSize { get; set; }
-
     public int MinBatchSize { get; set; }
 
     public int MaxBatchSize { get; set; }
-
-    public int DelayInMilliSeconds { get; set; }
 
     public int MinDelayInMilliSeconds { get; set; }
 
